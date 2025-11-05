7b8a95641ef76f880ef670b1e0b963c2839007c1
JesReyA <jesreyav@gmail.com>
2025-11-04 21:26:57 -0600

    Cambio de nombre

--- src/unam/fes/aragon/tercerparcial/problemauno/ArbolArchivo.java
+++ src/unam/fes/aragon/tercerparcial/problemauno/ArbolArchivo.java
@@ -1,8 +1,10 @@
 package unam.fes.aragon.tercerparcial.problemauno;
 
-public class pruebaArbolArreglo {
+public class ArbolArchivo {
     public static void main(String[] args) {
         ArbolBinarioArreglo<Integer> pruebaArbol = new ArbolBinarioArreglo<>();
+
+
 
         pruebaArbol.insertar(1231);
         pruebaArbol.imprimir();
