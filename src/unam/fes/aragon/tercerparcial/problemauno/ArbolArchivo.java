8907aa516dc394355b7c2f8a73b4a474d28004de
JesReyA <jesreyav@gmail.com>
2025-11-04 22:21:32 -0600

    Se crea lista con datos del txt, falta creación de árbol

--- src/unam/fes/aragon/tercerparcial/problemauno/ArbolArchivo.java
+++ src/unam/fes/aragon/tercerparcial/problemauno/ArbolArchivo.java
@@ -1,13 +1,32 @@
 package unam.fes.aragon.tercerparcial.problemauno;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ArbolArchivo {
     public static void main(String[] args) {
-        ArbolBinarioArreglo<Integer> pruebaArbol = new ArbolBinarioArreglo<>();
+        ManejoArchivos <String>  archivoIngresoDatosArbol = new ManejoArchivos<>();
+        FileReader archivo;
+        BufferedReader lector;
+        String cadena;
+        String[] datoIndividual;
+        ArbolBinarioArreglo<Integer> arbolCreado = new ArbolBinarioArreglo<>();
 
+        try{
+            archivo = new FileReader("DatosArbol.txt");
+            lector  = new BufferedReader(archivo);
 
+            while ((cadena = lector.readLine()) != null){
+                datoIndividual = cadena.split(" ");
+                for (String s : datoIndividual) {
+                    arbolCreado.insertar(Integer.parseInt(s));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
 
-        pruebaArbol.insertar(1231);
-        pruebaArbol.imprimir();
+        arbolCreado.imprimir();
     }
-
 }
